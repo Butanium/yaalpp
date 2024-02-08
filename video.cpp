5171068d57f9844cc5d488e9797cf22a07fa1f03
Hugo <hugoooo@crans.org>
2024-02-08 23:32:41 +0100

    adding video stream

---
+++ video.cpp
@@ -0,0 +1,136 @@
+#include "video.h"
+#include "opencv2/imgproc.hpp"
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <unsupported/Eigen/CXX11/Tensor>
+#include <mpi.h>
+
+#define FPS 16
+
+using namespace std;
+using namespace cv;
+using Eigen::Tensor;
+
+
+/**
+ * Convert an Eigen Tensor to an OpenCV Mat.
+ * @param tensor The tensor to convert.
+ * @return A pointer to the image matrix.
+ */
+Mat* eigen2cv(Tensor<float, 3> &tensor) {
+  Mat* image = new Mat(tensor.dimension(0), tensor.dimension(1), CV_8UC3);
+
+  for (int i = 0; i < tensor.dimension(0); i++) {
+    for (int j = 0; j < tensor.dimension(1); j++) {
+      BGR* pixel = image->ptr<BGR>(i, j);
+      pixel->blue = (unsigned char)(tensor(i, j, 0) * 255);
+      pixel->green = (unsigned char)(tensor(i, j, 1) * 255);
+      pixel->red = (unsigned char)(tensor(i, j, 2) * 255);
+    }
+  }
+
+  return image;
+}
+
+Stream::Stream(const char* filename, Size size, int workers_row, int workers_col, MPI_Comm comm) : filename(filename), workers_row(workers_row), workers_col(workers_col), comm(comm) {
+  int rank, comm_size;
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &comm_size);
+
+  if (comm_size != workers_row * workers_col + 1) {
+    if (rank == 0) {
+      cout << "The number of workers is not equal to workers_row * workers_col" << endl;
+    }
+    MPI_Abort(comm, 1);
+  }
+
+  Size worker_size(size.width / workers_col, size.height / workers_row);
+  this->worker_size = worker_size;
+
+  if (rank == 0) { // This is the disk writer
+    this->writer = new VideoWriter();
+    this->img_buffer = new Mat(size, CV_8UC3);
+    this->buffer = new uchar[this->img_buffer->total() * this->img_buffer->elemSize()]; // TODO : Maybe add a useless area to align the buffer
+    this->size = size;
+
+    this->recvcounts = new int[comm_size];
+    recvcounts[0] = 0;
+    for (int i = 1; i < comm_size; i++) {
+      recvcounts[i] = worker_size.area() * this->img_buffer->elemSize();
+    }
+
+    this->displs = new int[comm_size];
+    displs[0] = 0;
+    for (int i = 1; i < comm_size; i++) {
+      displs[i] = displs[i-1] + recvcounts[i-1];
+    }
+
+    int codec = VideoWriter::fourcc('a', 'v', 'c', '1');
+
+    this->writer->open(filename, codec, FPS, size, true);
+  } else { // This is a worker
+    this->writer = nullptr;
+    this->img_buffer = nullptr;
+    this->buffer = nullptr;
+
+    this->size = worker_size;
+  }
+}
+
+Stream::~Stream() {
+  if (this->writer != nullptr) {
+    delete this->writer;
+    delete this->img_buffer;
+    delete this->buffer;
+  }
+}
+
+void Stream::append_frame(Mat* frame) {
+  Mat resized;
+  resize(*frame, resized, this->size);
+
+  if (this->writer != nullptr) {
+    this->writer->write(resized);
+    return;
+  }
+
+  MPI_Gatherv(resized.data, resized.total() * resized.elemSize(), MPI_UNSIGNED_CHAR, nullptr, nullptr, nullptr, MPI_UNSIGNED_CHAR, 0, this->comm);
+}
+
+void Stream::append_frame(Tensor<float, 3> &frame) {
+  Mat* image = eigen2cv(frame);
+  this->append_frame(image);
+  delete image;
+}
+
+void Stream::write_frame() {
+  if (this->writer == nullptr) {
+    return;
+  }
+
+  int comm_size;
+  MPI_Comm_size(this->comm, &comm_size);
+
+  MPI_Gatherv(nullptr, 0, MPI_UNSIGNED_CHAR, this->buffer, this->recvcounts, this->displs, MPI_UNSIGNED_CHAR, 0, this->comm); // Gatherv because rank 0 only receive (no send)
+
+  for (int i = 1; i < comm_size; i++) {
+    Mat worker_frame(this->size, CV_8UC3, this->buffer + (i * this->worker_img_size * this->img_buffer->elemSize()));
+
+    Rect worker_rect( ((i-1) % this->workers_row) * this->worker_size.width, ((i-1) / 4) * this->worker_size.height, this->worker_size.width, this->worker_size.height);
+
+    worker_frame.copyTo( (*this->img_buffer)(worker_rect) );
+  }
+
+
+  this->writer->write(*this->img_buffer);
+}
+
+void Stream::end_stream() {
+  if (this->writer == nullptr) {
+    return;
+  }
+
+  this->writer->release();
+  cout << "Video written to " << this->filename << endl;
+}
---
+++ video.h
@@ -0,0 +1,86 @@
+#ifndef VIDEO_H 
+#define VIDEO_H
+
+#include <opencv2/opencv.hpp>
+#include <unsupported/Eigen/CXX11/Tensor>
+#include <mpi.h>
+
+struct BGR {
+  unsigned char blue;
+  unsigned char green;
+  unsigned char red;
+};
+
+// struct Stream {
+//   const char* filename;
+//   cv::VideoWriter* writer;
+//   cv::Size size;
+
+//   uchar* buffer;
+//   cv::Mat* img_buffer;
+
+//   size_t worker_img_size;
+//   MPI_Comm comm;
+// };
+
+class Stream {
+  private:
+    const char* filename;
+    cv::VideoWriter* writer;
+    cv::Size size;
+    cv::Size worker_size;
+    int workers_row;
+    int workers_col;
+
+    uchar* buffer;
+    cv::Mat* img_buffer;
+
+    int* recvcounts;
+    int* displs;
+
+
+    size_t worker_img_size;
+    MPI_Comm comm;
+
+  public:
+
+    /**
+     * Initialize a video stream.
+     * @param filename The name of the file to write to.
+     * @param size The size of the video final video.
+     * @param workers_row The number of workers per row.
+     * @param workers_col The number of workers per column.
+     * @param comm The MPI communicator : rank 0 is the disk writer, the other ranks are the workers which call append_frame.
+     */
+    Stream(const char* filename, cv::Size size, int workers_row, int workers_col, MPI_Comm comm);
+
+    /**
+     * Destroy a video stream.
+     */
+    ~Stream();
+
+    /**
+     * Append a frame to the video stream (write the frame to the disk).
+     * @param frame The frame to append.
+     */
+    void append_frame(cv::Mat* frame);
+
+    /**
+     * Append a frame to the video stream (write the frame to the disk).
+     * @param frame The frame to append.
+     */
+    void append_frame(Eigen::Tensor<float, 3> &frame);
+
+
+    /**
+     * Write a frame to the video stream. Used only by the disk writer in a multi-node setup, useless otherwise.
+     */
+    void write_frame();
+
+    /**
+     * End the video stream.
+     */
+    void end_stream();
+};
+
+#endif // !VIDEO_H
