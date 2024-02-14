63917309d9933b048a72e27b6e55bea0e31790f7
Parakewl <g.dhimoila@gmail.com>
2024-02-14 17:18:38 +0100

    cleaned main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
-#include <vector>
 #include <Eigen/Dense>
 #include <unsupported/Eigen/CXX11/Tensor>
 #include <argparse/argparse.hpp>
-
-#include "physics/quadtree.hpp"
-#include "physics/rect.hpp"
-#include "diffusion/separablefilter.hpp"
-#include <omp.h>
 #include <cstdio>
 /* Rough pseudo-code:
  * Tensor map = zeros({1000, 1000, 5});
@@ -70,8 +64,6 @@ void brain.forward(view) {
 using Eigen::Tensor;
 using Eigen::array;
 
-using Vec2 = Eigen::Vector2f;
-
 void parse_arguments(int argc, char *argv[], argparse::ArgumentParser &program) {
     program.add_description("Yet Another Artificial Life Program in cpp");
     program.add_argument("-H", "--height").help("Height of the map").default_value(1000).scan<'i', int>();
@@ -105,98 +97,4 @@ int main(int argc, char *argv[]) {
     // Print the 10*10*5 tensor
     std::cout << map.slice(array<Eigen::Index, 3>{0, 0, 0}, array<Eigen::Index, 3>{10, 10, 5})
               << std::endl;
-#ifdef _OPENMP
-    std::cout << "OpenMP is enabled" << std::endl;
-#endif
-    int seed = 42;
-    int n_points = 100000;
-    std::mt19937 generator(seed);
-    auto x_distr = std::uniform_real_distribution<float>(0, (float) 1);
-    auto y_distr = std::uniform_real_distribution<float>(0, (float) 1);
-
-    Rect rect(Vec2(0, 0), Vec2(1, 1));
-    QuadTree quadTree(std::move(rect), 4);
-
-    float t1 = omp_get_wtime();
-    Vec2* points;
-    points = (Vec2*) malloc(n_points * sizeof(Vec2));
-    for (int i = 0; i < n_points; i++) {
-        points[i] = Vec2(x_distr(generator), y_distr(generator));
-    }
-    float t2 = omp_get_wtime();
-    std::cout << "Creation took: " << t2 - t1 << " seconds" << std::endl;
-
-    float t3 = omp_get_wtime();
-#pragma omp parallel for default(none) shared(quadTree, points, n_points) schedule(static) num_threads(8)
-    for (int i = 0; i < n_points; i++) {
-        quadTree.insert(points[i]);
-    }
-    float t4 = omp_get_wtime();
-    std::cout << "Insertion took: " << t4 - t3 << " seconds" << std::endl;
-
-    float t5 = omp_get_wtime();
-    Vec2 *closests = new Vec2[n_points];
-    int errors = 0;
-#pragma omp parallel for default(none) shared(quadTree, points, closests, n_points, errors) schedule(static) num_threads(8)
-    for (int i = 0; i < n_points; i++) {
-        auto v = points[i];
-        std::optional<Vec2> closest = quadTree.closest(v);
-        if (closest.has_value()) {
-            closests[i] = closest.value();
-        } else {
-            // Yes this is not thread safe, but who cares (Catch2 requires are not thread safe)
-            errors++;
-        }
-    }
-    float t6 = omp_get_wtime();
-    std::cout << "Closest recursive took: " << t6 - t5 << " seconds and had " << errors << " errors" << std::endl;
-
-    float t7 = omp_get_wtime();
-    Vec2 *closests2 = new Vec2[n_points];
-    errors = 0;
-    for (int i = 0; i < n_points; i++) {
-        auto v = points[i];
-        std::optional<Vec2> closest = quadTree.closestIterative(v);
-        if (closest.has_value()) {
-            closests2[i] = closest.value();
-        } else {
-            // Yes this is not thread safe, but who cares (Catch2 requires are not thread safe)
-            errors++;
-        }
-    }
-    float t8 = omp_get_wtime();
-    std::cout << "Closest iterative took: " << t8 - t7 << " seconds and had " << errors << " errors" << std::endl;
-
-    free(points);
-    delete[] closests;
-    delete[] closests2;
-
-    /* timing diffusion
-     * */
-
-    /* correction of diffusion
-     * */
-    int minih = 4;
-    int miniw = 5;
-    int minic = 1;
-    int mini_filter_size = 5;
-
-    SeparableFilter mini_filter(mini_filter_size, minic);
-
-    Tensor<float, 3> mini_input(minih, miniw, minic);
-    mini_input.setZero();
-    mini_input(0, 0, 0) = 1;
-    mini_input(0, 1, 0) = 1;
-    mini_input(1, 0, 0) = 1;
-
-    std::cout << "Input: " << std::endl << mini_input.reshape(array<int, 2>{minih, miniw * minic}) << std::endl;
-
-    Tensor<float, 3> mini_output(minih, miniw, minic);
-    mini_filter.apply(mini_input, mini_output);
-    std::cout << "Output: " << std::endl << mini_output.reshape(array<int, 2>{minih, miniw * minic}) << std::endl;
-
-    mini_filter.apply_inplace(mini_input);
-    std::cout << "Inplace: " << std::endl << mini_input.reshape(array<int, 2>{minih, miniw * minic}) << std::endl;
-
-    return 0;
 }
