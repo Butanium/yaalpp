16a4c2a2aa151f6dd8e4bbc03fac7297499b78c5
Hugo <hugoooo@crans.org>
2024-03-18 22:10:42 +0100

    cleaning gpu diffusion test

--- tests/test.cpp
+++ tests/test.cpp
@@ -426,7 +426,7 @@ TEST_CASE("ENVIRONMENT") {
     }
 }
 
-TEST_CASE("DIF") {
+TEST_CASE("DIFFUSION") {
         using Constants::Yaal::MAX_SIZE;
         std::vector<float> diffusion_factors = {0., 0., 0., 2};
         std::vector<float> max_values = {1, 1, 1, 1};
@@ -439,7 +439,7 @@ TEST_CASE("DIF") {
         Environment env(height, width, 4, decay_factors, diffusion_factors, max_values);
         Stream stream("test_output/tt2/env_steps.mp4", 10, cv::Size(1000, 1000), 1, 1, false, MPI_COMM_WORLD);
         env.create_yaals_and_plants(num_yaal, num_plant);
-	env.map(15,15,3) = 1;
+        env.map(15,15,3) = 1;
         for (int i = 0; i < num_steps; i++) {
             env.step();
         }
@@ -454,12 +454,6 @@ TEST_CASE("DIF") {
 
         env3.diffusion_filter.use_cuda = true; // Set filter on gpu
 
-	std::cout << "filter row :: " << env.diffusion_filter.row_filters_transpose << std::endl;
-	std::cout << "filter col :: " << env.diffusion_filter.col_filters_transpose << std::endl;
-
-	Tensor<float, 2> envmap_chip = env.map.chip(3,2);
-	std::cout << "env map :: " << envmap_chip << std::endl;
-
         int num_steps2 = 1;
         for (int i = 0; i < num_steps2; i++) {
             env2.step();
@@ -469,8 +463,8 @@ TEST_CASE("DIF") {
             env3.step();
         }
 
-	std::string frame2 = "test_output/tt2/frame_env2.png";
-	std::string frame3 = "test_output/tt2/frame_env3.png";
+        std::string frame2 = "test_output/tt2/frame_env2.png";
+        std::string frame3 = "test_output/tt2/frame_env3.png";
         auto fov = Constants::Yaal::MAX_FIELD_OF_VIEW;
         Tensor<float, 3> reshaped_map2 = env2.map.slice(array<Index, 3>{fov, fov, 1},
                                                       array<Index, 3>{height, width, 3});
@@ -479,11 +473,6 @@ TEST_CASE("DIF") {
         stream.append_frame(reshaped_map2, frame2.c_str());
         stream.append_frame(reshaped_map3, frame3.c_str());
 
-	Tensor<float, 2> envmap2_chip = env2.map.chip(3,2);
-	std::cout << "env2 map :: " << envmap2_chip << std::endl;
-
-	Tensor<float, 2> envmap3_chip = env3.map.chip(3,2);
-	std::cout << "env3(gpu) map :: " << envmap3_chip << std::endl;
         // use is_close :
         REQUIRE(is_close(env2.map, env3.map));
 }
